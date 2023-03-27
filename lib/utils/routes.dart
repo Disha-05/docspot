d703513ce9a63dda539681d0793b2a8d33b56659
Disha-05 <77150251+Disha-05@users.noreply.github.com>
2023-03-27 13:02:15 +0530

    Changes added

---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+class MyRoutes {
+  static String loginRoute = "/home";
+  static String signupRoute = "/signup";
+  static String mainRoute = "/mainpage";
+  static String formRoute = "/formpage";
+}
